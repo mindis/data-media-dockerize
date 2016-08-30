6645b408eb2e2e47acba49edfe8a29528cdbb766
Marat Bunyatov <Marat@Magnetic.com>
2016-08-30 16:58:55 -0400

    python env

--- behave/steps/tutorial.py
+++ behave/steps/tutorial.py
@@ -2,13 +2,10 @@ from behave import *
 
 @given('we have behave installed')
 def step_impl(context):
-    context.browser.get('http://google.com')
     pass
 
 @when('we implement a test')
 def step_impl(context):
-    elements = find_elements(context.browser, id='no-account')
-    print(elements)
     assert True is not False
 
 @then('behave will test it for us!')
